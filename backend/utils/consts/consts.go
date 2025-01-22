b2f5f8eb027f581b5eeb82087207997f1cee2807
rayfiyo <rayfiyorex@gmail.com>
2025-01-23 05:25:47 +0900

    feat: 🍻 gemini model の追加とディレクトリ変更

---
+++ backend/utils/consts/consts.go
@@ -0,0 +1,10 @@
+package consts
+
+const (
+	SpecifyingResponseFormat string = `このコミュニティの文化を新しい方向に進化させるアイデアを提案してください。あなたの出力は **必ず次のJSON形式** で返してください。
+{
+    "newCulture": "string",
+    "populationChange": 0
+}"`
+	GeminiModel string = "gemini-2.0-flash-exp"
+)
